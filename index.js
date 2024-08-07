340071683016f780c1834d5c799767c8808c314b
ALADIN <aladin@iMac-de-ALADIN.local>
2024-08-07 15:17:25 +0000

    crée une barre de rechercher avec animation reduction du width

---
+++ index.js
@@ -0,0 +1,7 @@
+const contairEL = document.querySelector(".search-bar-contaire")
+
+const searchEl = document.querySelector('.mag')
+
+searchEl.addEventListener("click", () =>{
+    contairEL.classList.toggle("active")
+})
